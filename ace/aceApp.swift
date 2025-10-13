b7001779e9128a502e76e604e353e7dba4576178
kluczi <bartekcsgo04@gmail.com>
2025-10-13 17:10:18 +0200

    Initial Commit

---
+++ ace/aceApp.swift
@@ -0,0 +1,17 @@
+//
+//  aceApp.swift
+//  ace
+//
+//  Created by Bartek on 13/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct aceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ aceTests/aceTests.swift
@@ -0,0 +1,17 @@
+//
+//  aceTests.swift
+//  aceTests
+//
+//  Created by Bartek on 13/10/2025.
+//
+
+import Testing
+@testable import ace
+
+struct aceTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
