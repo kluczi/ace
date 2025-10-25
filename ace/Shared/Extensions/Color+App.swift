3ac9c02a2b87b3f9bf26f349c382b9a70120aa52
kluczi <bartekcsgo04@gmail.com>
2025-10-25 17:43:08 +0200

    added color extension from assets in Color+app.swift

---
+++ ace/Shared/Extensions/Color+App.swift
@@ -0,0 +1,21 @@
+//
+//  Color+App.swift
+//  ace
+//
+//  Created by Bartek on 25/10/2025.
+//
+
+import SwiftUI
+
+extension Color {
+    static let appPrimary = Color("PrimaryAppColor")
+    static let appSecondary = Color("SecondaryAppColor")
+    static let appBackground = Color("BackgroundColor")
+    static let appForeground = Color("ForegroundColor")
+    static let appSecondaryForeground=Color("SecondaryForegroundColor")
+    static let appBorder = Color("BorderColor")
+    static let appWin=Color("WinColor")
+    static let appLoss=Color("LossColor")
+    
+    
+}
