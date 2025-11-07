35236db6b146529160b25809035337640750f061
kluczi <bartekcsgo04@gmail.com>
2025-11-07 22:42:52 +0100

    added UserProfile model

---
+++ ace/Shared/Models/UserProfile.swift
@@ -0,0 +1,34 @@
+//
+//  UserProfile.swift
+//  ace
+//
+//  Created by Bartek on 07/11/2025.
+//
+
+import Foundation
+
+struct UserProfile: Codable, Identifiable, Equatable {
+    let id: String
+    let createdAt: Date
+    let updatedAt: Date?
+
+    var displayName: String
+    var handedness: String?
+    var country: String?
+    var city: String?
+    var rating: Double?
+    var avatarFileName: String?
+
+    enum Handedness: String, Codable {
+        case left, right, both
+    }
+
+    enum CodingKeys: String, CodingKey {
+
+        case id = "$id"
+        case createdAt = "$createdAt"
+        case updatedAt = "$updatedAt"
+        case displayName, handedness, country, rating, avatarFileName, city
+    }
+
+}
